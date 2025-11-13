ec621a707476a7c6c6963e5c6630e07fc87e433c
cboh4 <bohdan@scrtlabs.com>
2025-11-13 13:15:34 +0200

    Revert "Merge pull request #131 from scrtlabs/add-implicit-hash-to-header"
    This reverts commit 26f71adb761184702e1aa95791e27b7c06947a88, reversing
    changes made to 90a50c0421d672077d563a46bfc2a30671f56193.

--- state/execution.go
+++ state/execution.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"time"
-
 	abci "github.com/cometbft/cometbft/abci/types"
 	cryptoenc "github.com/cometbft/cometbft/crypto/encoding"
 	"github.com/cometbft/cometbft/libs/fail"
@@ -16,6 +14,7 @@ import (
 	"github.com/cometbft/cometbft/proxy"
 	"github.com/cometbft/cometbft/types"
 	tmenclave "github.com/scrtlabs/tm-secret-enclave"
+	"time"
 )
 
 //-----------------------------------------------------------------------------
@@ -139,16 +138,6 @@ func (blockExec *BlockExecutor) CreateProposalBlock(
 			Time:               block.Time,
 			NextValidatorsHash: block.NextValidatorsHash,
 			ProposerAddress:    block.ProposerAddress,
-			ImplicitHash:       block.ImplicitHash,
-			Version:            block.Version,
-			DataHash:           block.DataHash,
-			LastBlockId:        block.LastBlockID.ToProto(),
-			LastCommitHash:     block.LastCommitHash,
-			ValidatorsHash:     block.ValidatorsHash,
-			ConsensusHash:      block.ConsensusHash,
-			LastResultsHash:    block.LastResultsHash,
-			EvidenceHash:       block.EvidenceHash,
-			EncryptedRandom:    block.EncryptedRandom.ToProto(),
 		},
 	)
 	if err != nil {
@@ -184,16 +173,6 @@ func (blockExec *BlockExecutor) ProcessProposal(
 		Misbehavior:        block.Evidence.Evidence.ToABCI(),
 		ProposerAddress:    block.ProposerAddress,
 		NextValidatorsHash: block.NextValidatorsHash,
-		ImplicitHash:       block.ImplicitHash,
-		Version:            block.Version,
-		DataHash:           block.DataHash,
-		LastBlockId:        block.LastBlockID.ToProto(),
-		LastCommitHash:     block.LastCommitHash,
-		ValidatorsHash:     block.ValidatorsHash,
-		ConsensusHash:      block.ConsensusHash,
-		LastResultsHash:    block.LastResultsHash,
-		EvidenceHash:       block.EvidenceHash,
-		EncryptedRandom:    block.EncryptedRandom.ToProto(),
 	})
 	if err != nil {
 		return false, err
@@ -274,15 +253,6 @@ func (blockExec *BlockExecutor) applyBlock(state State, blockID types.BlockID, b
 		Txs:                block.Txs.ToSliceOfBytes(),
 		EncryptedRandom:    block.EncryptedRandom.ToProto(),
 		Commit:             blockExec.blockStore.LoadSeenCommit(block.Height).ToProto(),
-		ImplicitHash:       block.ImplicitHash,
-		Version:            block.Version,
-		DataHash:           block.DataHash,
-		LastBlockId:        block.LastBlockID.ToProto(),
-		LastCommitHash:     block.LastCommitHash,
-		ValidatorsHash:     block.ValidatorsHash,
-		ConsensusHash:      block.ConsensusHash,
-		LastResultsHash:    block.LastResultsHash,
-		EvidenceHash:       block.EvidenceHash,
 	})
 	endTime := time.Now().UnixNano()
 	blockExec.metrics.BlockProcessingTime.Observe(float64(endTime-startTime) / 1000000)
@@ -397,15 +367,6 @@ func (blockExec *BlockExecutor) ExtendVote(
 		Misbehavior:        block.Evidence.Evidence.ToABCI(),
 		NextValidatorsHash: block.NextValidatorsHash,
 		ProposerAddress:    block.ProposerAddress,
-		ImplicitHash:       block.ImplicitHash,
-		Version:            block.Version,
-		DataHash:           block.DataHash,
-		LastBlockId:        block.LastBlockID.ToProto(),
-		LastCommitHash:     block.LastCommitHash,
-		ValidatorsHash:     block.ValidatorsHash,
-		ConsensusHash:      block.ConsensusHash,
-		LastResultsHash:    block.LastResultsHash,
-		EvidenceHash:       block.EvidenceHash,
 	}
 
 	resp, err := blockExec.proxyApp.ExtendVote(ctx, &req)
@@ -808,16 +769,6 @@ func ExecCommitBlock(
 		DecidedLastCommit:  commitInfo,
 		Misbehavior:        block.Evidence.Evidence.ToABCI(),
 		Txs:                block.Txs.ToSliceOfBytes(),
-		ImplicitHash:       block.ImplicitHash,
-		Version:            block.Version,
-		DataHash:           block.DataHash,
-		LastBlockId:        block.LastBlockID.ToProto(),
-		LastCommitHash:     block.LastCommitHash,
-		ValidatorsHash:     block.ValidatorsHash,
-		ConsensusHash:      block.ConsensusHash,
-		LastResultsHash:    block.LastResultsHash,
-		EvidenceHash:       block.EvidenceHash,
-		EncryptedRandom:    block.EncryptedRandom.ToProto(),
 	})
 	if err != nil {
 		logger.Error("error in proxyAppConn.FinalizeBlock", "err", err)
--- state/state.go
+++ state/state.go
@@ -2,20 +2,19 @@ package state
 
 import (
 	"bytes"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"os"
 	"time"
-
+	"encoding/hex"
 	"github.com/cosmos/gogoproto/proto"
 
+	tmenclave "github.com/scrtlabs/tm-secret-enclave"
 	cmtstate "github.com/cometbft/cometbft/proto/tendermint/state"
 	cmtversion "github.com/cometbft/cometbft/proto/tendermint/version"
 	"github.com/cometbft/cometbft/types"
 	cmttime "github.com/cometbft/cometbft/types/time"
 	"github.com/cometbft/cometbft/version"
-	tmenclave "github.com/scrtlabs/tm-secret-enclave"
 )
 
 // database keys
@@ -279,11 +278,6 @@ func (state State) MakeBlock(
 		// println("Invalid random generated")
 		panic("Failed to validate generated random")
 	}
-
-	implicitHash, err := tmenclave.GetImplicitHash()
-	if err != nil {
-		panic("Failed to get implicit hash")
-	}
 	// ScrtLabs changes out <-
 
 	// Fill rest of header with state data.
@@ -292,10 +286,9 @@ func (state State) MakeBlock(
 		timestamp, state.LastBlockID,
 		state.Validators.Hash(), state.NextValidators.Hash(),
 		state.ConsensusParams.Hash(), state.AppHash, state.LastResultsHash,
-		proposerAddress, &encryptedRandom, implicitHash,
+		proposerAddress, &encryptedRandom,
 	)
 
-	fmt.Printf("------------ HEADER HASH : %+v\n", block.Header.Hash())
 	return block
 }
 
